a22b9f1be8b53de958b03adbf8e1e0a7d2ba7c1b
Nekspire <nekspire@gmail.com>
2021-03-16 14:48:03 +0100

    -Added licens files -Modified Cargo.toml adding some definitions for crates.io.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-
+![crates.io][crates-io-badge]
+![docs.rs][docs-rs-badge]
 ![No Std][no-std-badge]
+![downloads][crates-io-download-badge]
+
 # TEA5767
 Rust platform agnostic driver for the TEA5767, electronically tuned FM stereo radio. 
 An additional description can be found in the device datasheet.
-The datasheet is placed in doc folder of this repository.
+The datasheet is placed in [`doc`] folder of this repository.
 The driver is based on [`embedded-hal`] traits and I2C.
 
 ![](images/TEA5767.jpeg)
@@ -12,7 +15,7 @@ The driver is based on [`embedded-hal`] traits and I2C.
 General initialization code, which create new instance of TEA5767 with default configuration.
 Here you can specify radio channel frequency, band limit, and sound mode. To change this default
 behaviour and to control the device use methods defined in _defs_ module. Additional information
-about this methods can be obtained from examples folder.
+about this methods can be obtained from [`examples`] folder.
 ```rust
 use tea5767::defs::*;
 let radio_tuner = TEA5767::new(
@@ -46,6 +49,11 @@ for inclusion in the work by you, as defined in the Apache-2.0 license, shall
 be dual licensed as above, without any additional terms or conditions.
 
 [`embedded-hal`]: https://github.com/rust-embedded/embedded-hal
+[`doc`]: https://github.com/Nekspire/tea5767/tree/master/doc
+[`examples`]: https://github.com/Nekspire/tea5767/tree/master/examples
 
 <!-- Badges -->
 [no-std-badge]: https://img.shields.io/badge/no__std-yes-blue
+[crates-io-badge]: https://img.shields.io/crates/v/...
+[docs-rs-badge]: https://docs.rs/.../badge.svg
+[crates-io-download-badge]: https://img.shields.io/crates/d/...svg?maxAge=3600
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
+//! ![crates.io][crates-io-badge]
+//! ![docs.rs][docs-rs-badge]
 //! ![No Std][no-std-badge]
+//! ![downloads][crates-io-download-badge]
+//!
 //! # TEA5767
 //! Rust platform agnostic driver for the TEA5767, electronically tuned FM stereo radio.
-//! An additional description can be found in the device datasheet.
-//! The datasheet is placed in doc folder of this repository.
-//! The driver is based on [`embedded-hal`] traits and I2C.
 //!
+//! An additional description can be found in the device datasheet.
+//! The datasheet is placed in [`doc`] folder of this repository.
+//! The driver is based on [`embedded-hal`] traits and I2C.
+//! 
 //! ![](images/TEA5767.jpeg)
 //!
 //! ## Usage
 //! General initialization code, which create new instance of TEA5767 with default configuration.
 //! Here you can specify radio channel frequency, band limit, and sound mode. To change this default
 //! behaviour and to control the device use methods defined in _defs_ module. Additional information
-//! about this methods can be obtained from examples folder.
+//! about this methods can be obtained from [`examples`] folder.
 //! ```rust
 //! use tea5767::defs::*;
 //!     let radio_tuner = TEA5767::new(
@@ -45,9 +50,14 @@
 //! be dual licensed as above, without any additional terms or conditions.
 //!
 //! [`embedded-hal`]: https://github.com/rust-embedded/embedded-hal
+//! [`doc`]: https://github.com/Nekspire/tea5767/tree/master/doc
+//! [`examples`]: https://github.com/Nekspire/tea5767/tree/master/examples
 //!
 //! <!-- Badges -->
 //! [no-std-badge]: https://img.shields.io/badge/no__std-yes-blue
+//! [crates-io-badge]: https://img.shields.io/crates/v/...
+//! [docs-rs-badge]: https://docs.rs/.../badge.svg
+//! [crates-io-download-badge]: https://img.shields.io/crates/d/...svg?maxAge=3600
 
 #![no_std]
 //#![allow(unused)] // 1st stage of development
